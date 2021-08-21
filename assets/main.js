f041e23c0ac08cded9515323d192f754a2b08565
Emily Rendleman <emilyrendleman@gmail.com>
2021-08-21 16:04:21 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,28 @@
+// header fade out on scroll
+let header = document.getElementById('header');
+
+function fadeOutOnScroll(element) {
+	if (!element) {
+		return;
+	}
+	
+	let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+	let elementHeight = element.offsetHeight;
+	let scrollTop = document.documentElement.scrollTop;
+	
+	let opacity = 1;
+	
+	if (scrollTop > distanceToTop) {
+		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+	}
+	
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
+}
+
+function scrollHandler() {
+	fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
